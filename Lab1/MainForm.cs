84342014427105ccae9ee7dcbe1f34a2c83668be
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-12-05 01:03:21 +0400

    modify logic

--- Lab1/MainForm.cs
+++ Lab1/MainForm.cs
@@ -6,18 +6,71 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CommonLibrary;
 
 namespace Lab1
 {
     public partial class MainForm : Form
     {
+        private CongruentialGenerator generator = null;
         public MainForm()
         {
             InitializeComponent();
-            var hash = CommonLibrary.MaHash8v64.GetHashCode("привет");
-            var gen = new CommonLibrary.CongruentialGenerator(hash);
-            var res = gen.Next(255);
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (passTB.Text == "")
+            {
+                hashTB.Text = "";
+                return;
+            }
+            var hash = MaHash8v64.GetHashCode(passTB.Text);
+            hashTB.Text = hash + "";
+        }
+
+        private void genB_Click(object sender, EventArgs e)
+        {
+            if (hashTB.Text == "")
+            {
+                MessageBox.Show("Задайте пароль!");
+                return;
+            }
+            if (generator == null)
+                generator = new CongruentialGenerator(uint.Parse(hashTB.Text));
+            try
+            {
+                ulong res = 0;
+                if (minValueTB.Text != "")
+                {
+                    if (maxValueTB.Text == "")
+                    {
+                        MessageBox.Show("Задайте максимальное значение");
+                        return;
+                    }
+                    res = generator.Next(ulong.Parse(minValueTB.Text), ulong.Parse(maxValueTB.Text));
+                }
+                else
+                {
+                    res = maxValueTB.Text != "" ? generator.Next(ulong.Parse(maxValueTB.Text)) : generator.Next();
+                }
+                randomTB.Text = res + "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void onlyDigit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            if (e.KeyChar != (char)Keys.Back && e.KeyChar != '-')
+                e.Handled = true;
+            else if (e.KeyChar == '-')
+                if ((sender as TextBox).Text.Length != 0)
+                    e.Handled = true;
         }
     }
 }
