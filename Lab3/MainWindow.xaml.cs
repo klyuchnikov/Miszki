55eaeb31274a0f3a8cedb7772f4c6ad163f8cfaf
Дмитрий Ключников <klyuchnikovds@gmail.com>
2012-12-15 00:24:34 +0400

    add new project lab3

---
+++ Lab3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CommonLibrary;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var str = "qwe";
+            var bytes = Encoding.UTF8.GetBytes(str);
+            var data = new BigInteger(bytes);
+            var res = BigIntegerExtensions.GenRSAValues(16);
+
+            var rr = RSAEx.EnCrypt(data, res.Item1, res.Item2);
+            var ww = RSAEx.DeCrypt(rr, res.Item3, res.Item4);
+            var sss = Encoding.UTF8.GetString(ww.ToByteArray());
+        }
+    }
+}
